5fbaad823529c4b260e25a260367d0123a222855
Tina Chen <chen.tna@gmail.com>
2011-10-03 16:18:18 -0700

    changed manifest

--- src/com/uiproject/meetingplanner/MeetingPlanner.java
+++ src/com/uiproject/meetingplanner/MeetingPlanner.java
@@ -1,13 +1,20 @@
 package com.uiproject.meetingplanner;
 
+import com.google.android.maps.MapActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MeetingPlanner extends Activity {
+public class MeetingPlanner extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
