32bc1e2352295dfadfd9e53e3869c63c7f9419b8
Yuwen Bian <luckbian@hotmail.com>
2011-10-07 18:13:45 -0700

    Add the communication method from device to server

--- src/com/uiproject/meetingplanner/MeetingPlanner.java
+++ src/com/uiproject/meetingplanner/MeetingPlanner.java
@@ -1,7 +1,9 @@
 package com.uiproject.meetingplanner;    
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -165,5 +167,36 @@ public class MeetingPlanner extends MapActivity {
     		return "Oh no! We could not find the ETA. Please try again."; 
     	}
     }
+    
+    
+    /*Author:Yuwen
+     @Use this method to send your information
+     @The detailed info is stored individually into different params.
+     @And this method will also handling the response from the server.
+     */
+	private String getResponseResult() {
+		String param1="",param2="";
+		String urlStr = "http://XXXXXXXX/XXXXX/SendServlet?param1="+param1+"&param2="+param2;
+		//request.getParameter("param1");
+		String responseResult="";
+		try {
+			URL objUrl = new URL(urlStr);
+			URLConnection connect1 = objUrl.openConnection();
+			connect1.connect();
+			BufferedReader in = new BufferedReader(new InputStreamReader(connect1.getInputStream()));
+			//Data
+			String content;
+		
+			while((content=in.readLine())!=null)
+			{
+				responseResult+=content;
+			}
+			in.close();
+		} catch (Exception e) {
+			System.out.println("error!");
+		}
+		return responseResult;
+	}   	
+    
 }
 
