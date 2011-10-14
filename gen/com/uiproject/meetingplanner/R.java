35ffccb17df332fa3a69185d80307dc36b1c6eaa
Elizabeth Deng <elizabethdeng@gmail.com>
2011-10-14 14:04:37 -0700

    create "create meeting" activity

--- gen/com/uiproject/meetingplanner/R.java
+++ gen/com/uiproject/meetingplanner/R.java
@@ -8,6 +8,9 @@
 package com.uiproject.meetingplanner;
 
 public final class R {
+    public static final class array {
+        public static final int tracker_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,20 +18,40 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int eta=0x7f060002;
-        public static final int mapview=0x7f060003;
-        public static final int name=0x7f060001;
-        public static final int pic=0x7f060000;
+        public static final int addPeople=0x7f070012;
+        public static final int dateDisplay=0x7f070007;
+        public static final int date_label=0x7f070006;
+        public static final int endPickTime=0x7f07000e;
+        public static final int endTimeDisplay=0x7f07000d;
+        public static final int endTime_label=0x7f07000c;
+        public static final int eta=0x7f070002;
+        public static final int form_label=0x7f070003;
+        public static final int mapview=0x7f070013;
+        public static final int mname_field=0x7f070005;
+        public static final int mname_label=0x7f070004;
+        public static final int name=0x7f070001;
+        public static final int pic=0x7f070000;
+        public static final int pickDate=0x7f070008;
+        public static final int selectLocation=0x7f070011;
+        public static final int selectlocview=0x7f070014;
+        public static final int spinner_label1=0x7f07000f;
+        public static final int startPickTime=0x7f07000b;
+        public static final int startTimeDisplay=0x7f07000a;
+        public static final int startTime_label=0x7f070009;
+        public static final int tracker_selector=0x7f070010;
     }
     public static final class layout {
         public static final int attendeedialog=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int createmeeting=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int selectlocation=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int tracker_prompt=0x7f040002;
     }
     public static final class style {
-        public static final int AttendeeDialogTheme=0x7f050000;
+        public static final int AttendeeDialogTheme=0x7f060000;
     }
 }
