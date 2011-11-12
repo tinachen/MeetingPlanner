1dc31719d009ae2d29a2c1286d877e86844c2388
mengfei xu <mengfeixu@gmail.com>
2011-11-12 14:15:49 -0800

    Add parsers

--- src/com/uiproject/meetingplanner/Communicator.java
+++ src/com/uiproject/meetingplanner/Communicator.java
@@ -1,13 +1,14 @@
 package com.uiproject.meetingplanner;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.ParseException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.json.*;
 
@@ -49,11 +50,11 @@ public class Communicator {
 	public static int logIn(long phoneNumber, String password) {
 		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/myverifyuser?phoneNumber=" + phoneNumber + "&password=" + password;
 		String result = getResponseResult(urlStr);
-		
+
 		// debug msg
 		String tag = "Communicator";
 		Log.v(tag, urlStr);
-		
+
 		return Integer.valueOf(result);
 	}
 
@@ -66,15 +67,15 @@ public class Communicator {
 		String result = getResponseResult(urlStr);
 		return Integer.valueOf(result);
 	}
-	
-	public static Map<Integer,UserInstance> getAllUsers() throws JSONException {
+
+	public static Map<Integer, UserInstance> getAllUsers() throws JSONException {
 		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/mygetallusers";
 		String result = getResponseResult(urlStr);
 		JSONObject users = new JSONObject(result);
 		JSONArray userIds = users.names();
 		JSONArray userInfos = users.toJSONArray(userIds);
-		Map<Integer,UserInstance> allUsers = new HashMap<Integer, UserInstance>();
-		for ( int i = 0; i<userInfos.length();i++) {
+		Map<Integer, UserInstance> allUsers = new HashMap<Integer, UserInstance>();
+		for (int i = 0; i < userInfos.length(); i++) {
 			int userID = userIds.getInt(i);
 			String userFirstName = userInfos.getJSONObject(i).getString("firstName");
 			String userLastName = userInfos.getJSONObject(i).getString("lastName");
@@ -82,22 +83,79 @@ public class Communicator {
 			String userPhone = String.valueOf(userInfos.getJSONObject(i).getLong("phoneNumber"));
 			int userLocationLon = userInfos.getJSONObject(i).getInt("lon");
 			int userLocationLat = userInfos.getJSONObject(i).getInt("lat");
-			UserInstance userInstance = new UserInstance(userID,userFirstName,userLastName,userEmail,userPhone,userLocationLon,userLocationLat);
+			UserInstance userInstance = new UserInstance(userID, userFirstName, userLastName, userEmail, userPhone, userLocationLon, userLocationLat);
 			allUsers.put(userID, userInstance);
 		}
-		/*System.out.println(allUsers.get(1).getUserID());
-		System.out.println(allUsers.get(1).getUserFirstName());
-		System.out.println(allUsers.get(1).getUserLastName());
-		System.out.println(allUsers.get(1).getUserEmail());
-		System.out.println(allUsers.get(1).getUserPhone());
-		System.out.println(allUsers.get(1).getUserLocationLat());
-		System.out.println(allUsers.get(1).getUserLocationLon());*/
+		/*
+		 * System.out.println(allUsers.get(1).getUserID());
+		 * System.out.println(allUsers.get(1).getUserFirstName());
+		 * System.out.println(allUsers.get(1).getUserLastName());
+		 * System.out.println(allUsers.get(1).getUserEmail());
+		 * System.out.println(allUsers.get(1).getUserPhone());
+		 * System.out.println(allUsers.get(1).getUserLocationLat());
+		 * System.out.println(allUsers.get(1).getUserLocationLon());
+		 */
 		return allUsers;
 	}
-	
+
 	public static String removeMeeting(int meetingId) {
-		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/removemeeting?meetingId="+meetingId;
+		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/removemeeting?meetingId=" + meetingId;
 		String result = getResponseResult(urlStr);
 		return result;
 	}
+
+	public static Map<Integer, MeetingInstance> getAllMeetings() throws ParseException, JSONException {
+		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/mygetallmeetings";
+		String result = getResponseResult(urlStr);
+		JSONObject meetings = new JSONObject(result);
+		JSONArray meetingIds = meetings.names();
+		if (meetingIds == null)
+			return null;
+		JSONArray meetingInfos = meetings.toJSONArray(meetingIds);
+		Map<Integer, MeetingInstance> allMeetings = new HashMap<Integer, MeetingInstance>();
+		for (int i = 0; i < meetingInfos.length(); i++) {
+			int meetingID = meetingIds.getInt(i);
+			int meetingLat = meetingInfos.getJSONObject(i).getInt("meetingLat");
+			int meetingLon = meetingInfos.getJSONObject(i).getInt("meetingLon");
+			String meetingTitle = meetingInfos.getJSONObject(i).getString("name");
+			String meetingDescription = meetingInfos.getJSONObject(i).getString("description");
+			String meetingAddress = meetingInfos.getJSONObject(i).getString("meetingAddress");
+			String meetingDate = meetingInfos.getJSONObject(i).getString("meetingDate");
+			String meetingStartTime = meetingInfos.getJSONObject(i).getString("meetingStartTime");
+			String meetingEndTime = meetingInfos.getJSONObject(i).getString("meetingEndTime");
+			int meetingTrackTime = meetingInfos.getJSONObject(i).getInt("meetingTrackTime");
+			JSONArray users = meetingInfos.getJSONObject(i).getJSONObject("users").names();
+			JSONArray userStatusArray = meetingInfos.getJSONObject(i).getJSONObject("users").toJSONArray(users);
+			Set<UserInstance> attendees = new HashSet<UserInstance>();
+			for (int j = 0; j < userStatusArray.length(); j++) {
+				if (userStatusArray.getInt(j) == 2)
+					attendees.add(new UserInstance(users.getInt(i), "attending"));
+				if (userStatusArray.getInt(j) == 3)
+					attendees.add(new UserInstance(users.getInt(i), "pending"));
+				if (userStatusArray.getInt(j) == 4)
+					attendees.add(new UserInstance(users.getInt(i), "decline"));
+			}
+			int meetingInitiatorID = meetingInfos.getJSONObject(i).getInt("userId");
+			allMeetings.put(meetingIds.getInt(i), new MeetingInstance(meetingID, meetingLat, meetingLon, meetingTitle, meetingDescription,
+					meetingAddress, meetingDate, meetingStartTime, meetingEndTime, meetingTrackTime, attendees, meetingInitiatorID));
+		}
+		return allMeetings;
+	}
+
+	public static int updateMeeting(int meetingId, int userId, String name, String description, int meetingLat, int meetingLon,
+			String meetingAddress, String meetingDate, String meetingStartTime, String meetingEndTime, int meetingTrackTime, String people) {
+		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/myupdatemeeting?meetingId=" + meetingId + "&userId=" + userId + "&name="
+				+ name + "&description=" + description + "&meetingLat=" + meetingLat + "&meetingLon=" + meetingLon + "&meetingAddress="
+				+ meetingAddress + "&meetingDate=" + meetingDate + "&meetingStartTime=" + meetingStartTime + "&meetingEndTime=" + meetingEndTime
+				+ "&meetingTrackTime=" + meetingTrackTime + "&people=" + people;
+		String result = getResponseResult(urlStr);
+		return Integer.valueOf(result);
+	}
+	
+	public static int updateUser(int userId, long phoneNumber, String firstName, String lastName, String email, String password){
+		String urlStr = "http://cs-server.usc.edu:21542/newwallapp/forms/myupdateuser?userId="+userId+"&phoneNumber="+phoneNumber+"&firstName="+firstName+"&lastName="+lastName+"&email="+email+"&password="+password;;
+		String result = getResponseResult(urlStr);
+		return Integer.valueOf(result);
+	}
+
 }
