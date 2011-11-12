d9a6367073b768d8e7540b00c166c68b93d036d8
Laura Rodriguez <lau.rodriguez07@gmail.com>
2011-11-11 16:27:51 -0800

    Adding an All-meetings wrapper for tabs

---
+++ src/com/uiproject/meetingplanner/AllMeetings.java
@@ -0,0 +1,30 @@
+package com.uiproject.meetingplanner;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class AllMeetings extends TabActivity{
+
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
+
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, MeetingList.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("artists").setIndicator("Artists")
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    tabHost.setCurrentTab(0);
+	}
+}
