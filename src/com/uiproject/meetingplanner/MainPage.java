5e9c2f8d9cfd0d79ca64d38e2ec796bd8d9ebfe6
Yuwen Bian <luckbian@hotmail.com>
2011-11-18 20:25:19 -0800

    receiver update

--- src/com/uiproject/meetingplanner/MainPage.java
+++ src/com/uiproject/meetingplanner/MainPage.java
@@ -131,29 +131,7 @@ public class MainPage extends Activity {
     	
     }
  //**************************************************************   
-    public class TestReceiver extends BroadcastReceiver { 
-    	public TestReceiver (){
-    	}
 
-        public void onReceive(Context context, Intent intent) { 
-            // TODO Auto-generated method stub       
-            Log.d ("Receiver","Success");
-            Bundle message = intent.getBundleExtra("message");
-            int tag = message.getInt("tag");
-            Bundle locations = message.getBundle("locations");
-            Map<Integer,UserInstance> userLocations = new HashMap<Integer, UserInstance>();
-            for (String i : locations.keySet()){
-            	Bundle location = locations.getBundle(i);
-            	userLocations.put(Integer.valueOf(i), new UserInstance(Integer.valueOf(i),location.getInt("lat"),location.getInt("lon"),location.getString("eta")));
-            }
-            Log.d("tag","tag: "+tag);
-            Log.d("AAA","userId: "+6);
-            Log.d("AAA","lat: "+userLocations.get(6).getUserLocationLat());
-            Log.d("AAA","lon: "+userLocations.get(6).getUserLocationLon());
-            Log.d("AAA","eta: "+userLocations.get(6).getUserEta());
-        } 
-    } 
-   
  //*******************************************************************
     public void createMeeting(View button){
 
@@ -162,10 +140,7 @@ public class MainPage extends Activity {
 	//	Intent intent = new Intent(MainPage.this, CommunicateService.class);
 	//	startService(intent);
 		
-		TestReceiver receiver =new TestReceiver();
-		IntentFilter filter = new IntentFilter();
-		filter.addAction("com.uiproject.meetingplanner");
-		registerReceiver(receiver, filter);
+		
 		
 		
 
