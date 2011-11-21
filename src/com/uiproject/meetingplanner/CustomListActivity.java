e368ca9d20912d0f1aeca44614f0cf7f5189b926
Laura Rodriguez <lau.rodriguez07@gmail.com>
2011-11-20 17:04:19 -0800

    More Changes to the Meeting List

--- src/com/uiproject/meetingplanner/CustomListActivity.java
+++ src/com/uiproject/meetingplanner/CustomListActivity.java
@@ -7,6 +7,7 @@ import com.uiproject.meetingplanner.database.MeetingPlannerDatabaseHelper;
 import com.uiproject.meetingplanner.database.MeetingPlannerDatabaseManager;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -44,24 +49,50 @@ public class CustomListActivity extends Activity {
     	allMeet = db.getAllMeetings();
     	db.close();
 
-      //  MeetingListArrayAdapter adapter = new MeetingListArrayAdapter(this, allMeet);
+    	//  MeetingListArrayAdapter adapter = new MeetingListArrayAdapter(this, allMeet);
         MeetingAdapter adapter = new MeetingAdapter(this,allMeet);
 
         list.setAdapter(adapter);
         
         bar = (View)findViewById(R.id.linearLayout1);
+        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        list.setItemsCanFocus(false);
+        list.setClickable(true);
+        list.setSelected(true);
+        /*
+        CheckBox cb = (CheckBox) findViewById(R.id.checkBox1); 
+        cb.setOnCheckedChangeListener(new OnCheckedChangeListener() { 
 
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(new OnItemClickListener() {
+        	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) { 
+        	// TODO Auto-generated method stub 
+            if (buttonView.isChecked()) { 
+            	Toast.makeText(getBaseContext(), "Checked", 
+            	Toast.LENGTH_SHORT).show(); 
+            } 
+            else 
+            { 
+            	Toast.makeText(getBaseContext(), "UnChecked", 
+            	Toast.LENGTH_SHORT).show(); 
+            } 
+          } 
+        }); 
+        */
+        list.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                    int arg2, long arg3) {
+                // TODO Auto-generated method stub
+            	Toast.makeText(getBaseContext(), "Checked", 
+                    	Toast.LENGTH_SHORT).show(); 
+            	Log.d("checked",""+arg2);
+            }
 
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				// TODO Auto-generated method stub
-				Log.d("ITEM","ITEM_CLICKED");
-			//	bar.setVisibility(View.VISIBLE);
-				hideBar();
-			}
-		});
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+
+        });
     }
+	
 	public static void showBar(){
 		bar.setVisibility(View.VISIBLE);
 	}
