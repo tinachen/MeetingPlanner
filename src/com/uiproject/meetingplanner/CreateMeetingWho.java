025ceb6cbf184e455d369d61b09f2562bd82d55a
Elizabeth Deng <elizabethdeng@gmail.com>
2011-11-21 03:47:42 -0800

    off by 1 error fix

--- src/com/uiproject/meetingplanner/CreateMeetingWho.java
+++ src/com/uiproject/meetingplanner/CreateMeetingWho.java
@@ -36,7 +36,7 @@ public class CreateMeetingWho extends Search {
     		}else{
 	    		u = uids.substring(0, commaIndex);
 	    		checkedUids.add(Integer.parseInt(u));
-	    		uids = uids.substring(commaIndex + 2);
+	    		uids = uids.substring(commaIndex + 1);
     		}
 		}
     	for (int i = 0; i < checkedUids.size(); i++)
