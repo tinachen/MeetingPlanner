216afeffeab2810b403213887f525b7307f52853
Tina Chen <chen.tna@gmail.com>
2011-11-21 21:29:23 -0800

    update old meeting list

--- src/com/uiproject/meetingplanner/MeetingListArrayAdapter.java
+++ src/com/uiproject/meetingplanner/MeetingListArrayAdapter.java
@@ -99,7 +99,6 @@ public class MeetingListArrayAdapter extends ArrayAdapter<MeetingInstance> {
 	            		
 	            		String phonenumber = u.getUserPhone();
 	            		
-	            		// MENGFEI'S CODE GOES HERE - TODO
 	            		Intent intent = new Intent(Intent.ACTION_CALL);
 	            		intent.setData(Uri.parse("tel:" + phonenumber));
 	            		context.startActivity(intent);
@@ -157,7 +156,11 @@ public class MeetingListArrayAdapter extends ArrayAdapter<MeetingInstance> {
 		holder.meetingTime.setText(list.get(position).getMeetingDate() + " " + list.get(position).getMeetingStartTime());
 		holder.checkbox.setChecked(list.get(position).isSelected());
 		holder.itemIcon.setImageResource(list.get(position).getMeetingImageResourceID());
-		if(uid == m.getMeetingInitiatorID() && listType == LISTTYPE_ACCEPTED){
+		
+		if(listType == LISTTYPE_OLD){
+			holder.checkbox.setVisibility(View.GONE);
+			holder.itemIcon.setVisibility(View.GONE);
+		}else if(uid == m.getMeetingInitiatorID() && listType == LISTTYPE_ACCEPTED){
 			holder.checkbox.setVisibility(View.INVISIBLE);
 		}else{
 			holder.checkbox.setVisibility(View.VISIBLE);
