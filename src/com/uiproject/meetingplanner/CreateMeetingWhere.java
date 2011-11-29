d04762ea829ce7c42bda6584a27b746fcc2fcbec
cauchych <cauchych@usc.edu>
2011-11-28 19:14:53 -0800

    changed layout on mainpage and in search

--- src/com/uiproject/meetingplanner/CreateMeetingWhere.java
+++ src/com/uiproject/meetingplanner/CreateMeetingWhere.java
@@ -188,14 +188,17 @@ public class CreateMeetingWhere extends SelectLocation implements OnEditorAction
 	        return true;
 	    }
 
+	    @Override
 	    public boolean onEditorAction(TextView v, int actionId,
 	            KeyEvent event) {
-	        if (event != null&& (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+	        if (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+	            findAddress(v);
+	            v.setSingleLine(true);
+	            Log.d("TESTTESTTEST", "TEST");
 	            InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 	            in.hideSoftInputFromWindow(v
 	                    .getApplicationWindowToken(),
 	                    InputMethodManager.HIDE_NOT_ALWAYS);
-	            findAddress(v);
 	        }
 	        return false;
 	    }
