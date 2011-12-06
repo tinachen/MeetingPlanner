1d558b71277ceb79ae381add85abc77756daf396
mengfei xu <mengfeixu@gmail.com>
2011-12-05 23:11:37 -0800

    Fix "taking a phone" in Signup.java and EditProfile.java

--- src/com/uiproject/meetingplanner/EditProfile.java
+++ src/com/uiproject/meetingplanner/EditProfile.java
@@ -1,18 +1,25 @@
 package com.uiproject.meetingplanner;
 
+import java.io.File;
+
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class EditProfile extends Activity {
@@ -20,89 +27,109 @@ public class EditProfile extends Activity {
 
 	EditText fname_field, lname_field, phone_field, email_field, oldpw_field, pw_field, pw2_field;
 	String fname, lname, phone, email;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	private ImageView editpic;
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.editprofile);
-        
-        fname_field = (EditText) findViewById(R.id.fname);
-        lname_field = (EditText) findViewById(R.id.lname);
-        phone_field = (EditText) findViewById(R.id.phone);
-        email_field = (EditText) findViewById(R.id.email);
-        oldpw_field = (EditText) findViewById(R.id.oldpw);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.editprofile);
+
+		fname_field = (EditText) findViewById(R.id.fname);
+		lname_field = (EditText) findViewById(R.id.lname);
+		phone_field = (EditText) findViewById(R.id.phone);
+		email_field = (EditText) findViewById(R.id.email);
+		oldpw_field = (EditText) findViewById(R.id.oldpw);
+		editpic = (ImageView) findViewById(R.id.editpic);
+
+		editpic.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				takePhoto(v);
+			}
+		});
 		SharedPreferences settings = getSharedPreferences(PREFERENCE_FILENAME, MODE_PRIVATE);
-    	
-        //get info from db
-        fname = settings.getString("userFirstName", "");
-        lname = settings.getString("userLastName", "");
-        phone = settings.getString("userPhoneNumber", "");
-        email = settings.getString("userEmail", "");
-        
-        // set fields
-        fname_field.setText(fname);
-        lname_field.setText(lname);
-        phone_field.setText(phone);
-        email_field.setText(email);
-        
+
+		// get info from db
+		fname = settings.getString("userFirstName", "");
+		lname = settings.getString("userLastName", "");
+		phone = settings.getString("userPhoneNumber", "");
+		email = settings.getString("userEmail", "");
+
+		// set fields
+		fname_field.setText(fname);
+		lname_field.setText(lname);
+		phone_field.setText(phone);
+		email_field.setText(email);
+
 	}
 
-	public void submit(View button){
-        
-        String fname = fname_field.getText().toString();
-        String lname = lname_field.getText().toString();
-        String phone = phone_field.getText().toString();
-        String email = email_field.getText().toString();
-        
-        if (fname.length() == 0 || lname.length() == 0 || phone.length() == 0 || email.length() == 0){
-            Toast.makeText(getBaseContext(), "Please fill in all fields (name, phone, email)", Toast.LENGTH_SHORT).show();
-        }
-        
+	public void submit(View button) {
+
+		String fname = fname_field.getText().toString();
+		String lname = lname_field.getText().toString();
+		String phone = phone_field.getText().toString();
+		String email = email_field.getText().toString();
+
+		if (fname.length() == 0 || lname.length() == 0 || phone.length() == 0 || email.length() == 0) {
+			Toast.makeText(getBaseContext(), "Please fill in all fields (name, phone, email)", Toast.LENGTH_SHORT).show();
+		}
 
 		SharedPreferences settings = getSharedPreferences(PREFERENCE_FILENAME, MODE_PRIVATE);
 		int uid = settings.getInt("uid", 0);
-		
+
 		SharedPreferences.Editor editor = settings.edit();
-    	editor.putString("userPhoneNumber", phone);
-    	editor.putString("userFirstName", fname);
-    	editor.putString("userLastName", lname);
-    	editor.putString("userEmail", email);
-    	editor.commit();
-    	
-        //TODO Communicator.updateUser(uid, Long.parseLong(phone), fname, lname, email, pw);
+		editor.putString("userPhoneNumber", phone);
+		editor.putString("userFirstName", fname);
+		editor.putString("userLastName", lname);
+		editor.putString("userEmail", email);
+		editor.commit();
 
-        EditProfile.this.setResult(R.string.edited_profile);
-        EditProfile.this.finish();
-	}
-	
-	public void changepassword(View button){
+		// TODO Communicator.updateUser(uid, Long.parseLong(phone), fname,
+		// lname, email, pw);
 
-        Intent intent = new Intent(EditProfile.this, ChangePassword.class);
-        EditProfile.this.startActivity(intent);
+		EditProfile.this.setResult(R.string.edited_profile);
+		EditProfile.this.finish();
 	}
-	
-    // menu 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.logoutonly, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.logout:{
-            	logout();
-            	break;
-            }
-        }
-        return true;
-    }
-    
-    private void logout(){
-    	Logout.logout(this);
-    }
+
+	public void changepassword(View button) {
+
+		Intent intent = new Intent(EditProfile.this, ChangePassword.class);
+		EditProfile.this.startActivity(intent);
+	}
+
+	// menu
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.logoutonly, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.logout: {
+			logout();
+			break;
+		}
+		}
+		return true;
+	}
+
+	private void logout() {
+		Logout.logout(this);
+	}
+
+	private Uri imageUri;
+	private static final int TAKE_PICTURE = 9;
+
+	public void takePhoto(View view) {
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+		imageUri = Uri.fromFile(photo);
+		startActivityForResult(intent, TAKE_PICTURE);
+	}
 
 }
--- src/com/uiproject/meetingplanner/Signup.java
+++ src/com/uiproject/meetingplanner/Signup.java
@@ -1,5 +1,6 @@
 package com.uiproject.meetingplanner;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 
@@ -17,13 +18,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Signup extends Activity {
@@ -34,120 +40,136 @@ public class Signup extends Activity {
 	private ProgressDialog dialog;
 	private SharedPreferences settings;
 	private Editor editor;
-	
+	private ImageView signuppic;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
-        
-        fname_field = (EditText) findViewById(R.id.fname);
-        lname_field = (EditText) findViewById(R.id.lname);
-        phone_field = (EditText) findViewById(R.id.phone);
-        email_field = (EditText) findViewById(R.id.email);
-        pw_field = (EditText) findViewById(R.id.pw);
-        pw2_field = (EditText) findViewById(R.id.pw2);
-        submit = (Button) findViewById(R.id.submit);
-        
-        //int colors[] = { 0xffffee66, 0xffffc941, 0xffffab23 };
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.signup);
 
-        //GradientDrawable g = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
-        //submit.setBackgroundDrawable(g);
-        
-        //get info from phone and input it into the fields here
-        
-        AccountManager accountManager = AccountManager.get(this); 
-        Account[] accounts = accountManager.getAccountsByType("com.google");
-        Account account;
-        if (accounts.length > 0) {
-          account = accounts[0];   
-          String email = account.name;
-          email_field.setText(email);
-          
-        }
-        
-        TelephonyManager tMgr =(TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-        String phone = tMgr.getLine1Number();
-        phone_field.setText(phone);
-        
-     
+		fname_field = (EditText) findViewById(R.id.fname);
+		lname_field = (EditText) findViewById(R.id.lname);
+		phone_field = (EditText) findViewById(R.id.phone);
+		email_field = (EditText) findViewById(R.id.email);
+		pw_field = (EditText) findViewById(R.id.pw);
+		pw2_field = (EditText) findViewById(R.id.pw2);
+		submit = (Button) findViewById(R.id.submit);
+		signuppic = (ImageView) findViewById(R.id.signuppic);
+
+		signuppic.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				takePhoto(v);
+			}
+		});
+
+		// int colors[] = { 0xffffee66, 0xffffc941, 0xffffab23 };
+
+		// GradientDrawable g = new
+		// GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
+		// submit.setBackgroundDrawable(g);
+
+		// get info from phone and input it into the fields here
+
+		AccountManager accountManager = AccountManager.get(this);
+		Account[] accounts = accountManager.getAccountsByType("com.google");
+		Account account;
+		if (accounts.length > 0) {
+			account = accounts[0];
+			String email = account.name;
+			email_field.setText(email);
+
+		}
+
+		TelephonyManager tMgr = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+		String phone = tMgr.getLine1Number();
+		phone_field.setText(phone);
+
 		settings = getSharedPreferences(PREFERENCE_FILENAME, MODE_PRIVATE);
 		editor = settings.edit();
-        
-        // Hook up with database
-	    db = new MeetingPlannerDatabaseManager(this, MeetingPlannerDatabaseHelper.DATABASE_VERSION);
-	    
+
+		// Hook up with database
+		db = new MeetingPlannerDatabaseManager(this, MeetingPlannerDatabaseHelper.DATABASE_VERSION);
+
 	}
 
-	public void submit(View button) throws JSONException, ParseException, UnsupportedEncodingException{
+	public void submit(View button) throws JSONException, ParseException, UnsupportedEncodingException {
 
-        //error checking here
-        
-        String fname = fname_field.getText().toString();
-        String lname = lname_field.getText().toString();
-        String phone = phone_field.getText().toString();
-        String email = email_field.getText().toString();
-        String pw = pw_field.getText().toString();
-        String pw2 = pw2_field.getText().toString();
-        
-        if (fname.length() == 0 || lname.length() == 0 || phone.length() == 0 || 
-        		email.length() == 0 || pw.length() == 0 || pw2.length() == 0){
-        	
+		// error checking here
 
-            Toast.makeText(getBaseContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        
-        if(!pw.equals(pw2)){
-            Toast.makeText(getBaseContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
-            return;
-        	
-        }
-        
-        long phonenumber = Long.parseLong(phone);
-        
-        // Send create user request to server
+		String fname = fname_field.getText().toString();
+		String lname = lname_field.getText().toString();
+		String phone = phone_field.getText().toString();
+		String email = email_field.getText().toString();
+		String pw = pw_field.getText().toString();
+		String pw2 = pw2_field.getText().toString();
+
+		if (fname.length() == 0 || lname.length() == 0 || phone.length() == 0 || email.length() == 0 || pw.length() == 0 || pw2.length() == 0) {
+
+			Toast.makeText(getBaseContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		if (!pw.equals(pw2)) {
+			Toast.makeText(getBaseContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+			return;
+
+		}
+
+		long phonenumber = Long.parseLong(phone);
+
+		// Send create user request to server
 		int uid = Communicator.createUser(phonenumber, fname, lname, email, pw);
-		
-		if(uid == -1){
+
+		if (uid == -1) {
 			// User has already been created, show error msg
 			Toast.makeText(getBaseContext(), "Phone number has already been used by other users!", Toast.LENGTH_SHORT).show();
 			return;
 		}
-		
+
 		// User has been created successfully
 		editor.putInt("uid", uid);
-    	editor.putString("userPhoneNumber", Long.toString(phonenumber));
-    	editor.putString("userFirstName", fname);
-    	editor.putString("userLastName", lname);
-    	editor.putString("userEmail", email);
+		editor.putString("userPhoneNumber", Long.toString(phonenumber));
+		editor.putString("userFirstName", fname);
+		editor.putString("userLastName", lname);
+		editor.putString("userEmail", email);
 		editor.commit();
-		
+
 		// Show the ProgressDialog on this thread
-        this.dialog = ProgressDialog.show(this, "", "Signing up...", true);
-        new StoreDataToDb(this, db).execute();
-		
-		
+		this.dialog = ProgressDialog.show(this, "", "Signing up...", true);
+		new StoreDataToDb(this, db).execute();
+
 	}
 
-	
-private class StoreDataToDb extends AsyncTask<Integer, Void, Void> {
-		
+	private Uri imageUri;
+	private static final int TAKE_PICTURE = 9;
+
+	public void takePhoto(View view) {
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+		imageUri = Uri.fromFile(photo);
+		startActivityForResult(intent, TAKE_PICTURE);
+	}
+
+	private class StoreDataToDb extends AsyncTask<Integer, Void, Void> {
+
 		private Context context;
 		private MeetingPlannerDatabaseManager db;
-		
-		public StoreDataToDb(Context context, MeetingPlannerDatabaseManager db){
+
+		public StoreDataToDb(Context context, MeetingPlannerDatabaseManager db) {
 			this.context = context;
 			this.db = db;
 		}
-		
+
 		@Override
 		protected Void doInBackground(Integer... params) {
-			
+
 			// Open db connection
 			db.open();
 			// Get meetings info & user infos from server and update internal db
-	    	try {
+			try {
 				MeetingPlannerDatabaseUtility.updateDatabase(db);
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
@@ -156,29 +178,28 @@ private class StoreDataToDb extends AsyncTask<Integer, Void, Void> {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			// Store user into internal db
-			//db.createUser(uid, fname, lname, email, Long.toString(phonenumber), 0, 0);
+			// db.createUser(uid, fname, lname, email,
+			// Long.toString(phonenumber), 0, 0);
 			db.close();
-			
-			
+
 			return null;
 		}
-		
-		protected void onPostExecute(Void v){
+
+		protected void onPostExecute(Void v) {
 			if (Signup.this.dialog != null) {
 				Signup.this.dialog.dismiss();
-            }
-			
+			}
+
 			// Notify user that the registration is successful
 			Toast.makeText(context, "You signed up successfully!", Toast.LENGTH_SHORT).show();
-			
-			
-	        // no problems, go to main page
-	        Intent intent = new Intent(context, MainPage.class);
-	        ((Activity)context).startActivity(intent);
+
+			// no problems, go to main page
+			Intent intent = new Intent(context, MainPage.class);
+			((Activity) context).startActivity(intent);
 		}
-		
+
 	}
 
 }
